8e0c3263ba011d86d6fd201629854429ca039e44
KIJ <dlrwn0730@naver.com>
2025-09-08 21:43:37 +0900

    .

--- src/sections/Instargram.jsx
+++ src/sections/Instargram.jsx
@@ -1,19 +1,32 @@
 import React from 'react'
+import instar from "../util/instar";
 
 const Instargram = () => {
     return (
-        <div className='inner instar-inner'>
+        <div className="inner instar-inner">
             <div className="t-wrap">
-
+                <h3 className="title">Instagram</h3>
+                <a
+                    className="more"
+                    href="https://www.instagram.com/tocobo_official"
+                    target="_blank"
+                    rel="noreferrer"
+                >
+                    @tocobo_official
+                </a>
             </div>
+
             <ul className="instar-lst">
-                {/* {} */}
-                <li>
-                    <a href="#"></a>
-                </li>
+                {instar.map(({ id, image, alt, link }) => (
+                    <li key={id}>
+                        <a href={link} aria-label={alt}>
+                            <img src={image} alt={alt} loading="lazy" />
+                        </a>
+                    </li>
+                ))}
             </ul>
         </div>
-    )
-}
+    );
+};
 
-export default Instargram
+export default Instargram;
